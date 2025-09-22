41a726493c177b21d4c72587c62efed57354f46e
Roc <rocdongai@gmail.com>
2025-09-22 21:13:45 +0800

    feat(ai): 添加对话记忆功能支持新增 `MessageChatMemoryAdvisor` 和 `SimpleLoggerAdvisor` 配置

--- src/main/java/org/example/config/AiConfig.java
+++ src/main/java/org/example/config/AiConfig.java
@@ -2,6 +2,10 @@ package org.example.config;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +17,11 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.defaultSystem("你是小滴课堂的超级智能助手 老王，可以回答IT任何技术问题")
+    public ChatClient chatClient(OpenAiChatModel chatModel, ChatMemory chatMemory) {
+        return ChatClient.builder(chatModel)
+                // 配置内存存储方案
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build(), new SimpleLoggerAdvisor())
+                .defaultSystem("你是小滴课堂的超级智能助手 老王，可以回答IT任何技术问题")
                 .build();
     }
 }
